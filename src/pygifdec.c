b3a35fbb9ca8fab3cfd9429d89e4cb2660fc92cf
alingse <alingse@foxmail.com>
2023-12-07 01:52:11 +0800

    add values

--- src/pygifdec.c
+++ src/pygifdec.c
@@ -2,24 +2,25 @@
 #include <object.h>
 #include "gifdec.h"
 
-static PyObject *add(PyObject *self, PyObject *args)
+typedef struct GIF
 {
-    double a, b, result;
-    if (!PyArg_ParseTuple(args, "dd", &a, &b))
-    {
-        return NULL;
-    }
-    result = a + b;
-    return Py_BuildValue("d", result);
+    PyObject_HEAD;
+    PyObject *width;
+    PyObject *height;
+    gd_GIF *gd_GIF_ptr;
+} GIF;
+
+static PyObject *GIF_close(PyObject *self, PyObject *args)
+{
+    GIF *gif = (GIF *)self;
+    // printf("in close function the gd_GIF pointer is %d \n", gif->gd_GIF_ptr);
+    gd_close_gif(gif->gd_GIF_ptr);
+    Py_RETURN_NONE;
 }
 
 static PyMethodDef methods_of_GIF[] = {
+    {"close", GIF_close, METH_NOARGS, "close the gif"},
     {NULL, NULL, 0, NULL}};
-
-typedef struct GIF
-{
-    uint8_t *gd_GIF;
-} GIF;
 
 static PyTypeObject PyGIF = {
     PyVarObject_HEAD_INIT(NULL, 0)
@@ -44,79 +45,14 @@ static PyObject *open_gif(PyObject *self, PyObject *args)
         Py_RETURN_NONE;
     }
 
-    GIF *result = PyObject_New(GIF, &PyGIF);
-    result->gd_GIF = gif;
-    /*
-    //  copy
-    result->fd = gif->fd;
-    result->anim_start = gif->anim_start;
-    result->width = gif->width;
-    uint16_t a = gif->height;
-    printf("gif size %zu \n", sizeof(gif));
-    printf("*gif size %zu \n", sizeof(*gif));
-    printf("result size %zu \n", sizeof(result));
-    printf("*result size %zu \n", sizeof(*result));
-    // result->height++;
-    //  result->height = 1;
-    result->depth;
-    result->height;
-    // = gif->depth;
-    */
-    /*
-
-
-result->loop_count = gif->loop_count;
-result->gce = gif->gce;
-result->lct = gif->lct;
-result->gct = gif->gct;
-result->plain_text = gif->plain_text;
-result->comment = gif->comment;
-result->application = gif->application;
-result->fx = gif->fx;
-result->fy = gif->fy;
-result->fw = gif->fw;
-result->fh = gif->fh;
-result->bgindex = gif->bgindex;
-*/
-    /*
-    // Copy the dynamically allocated memory (if any)
-    result->palette = malloc(sizeof(gd_Palette));
-    if (result->palette == NULL)
-    {
-        // Failed to allocate memory
-        free(result);
-        return NULL;
-    }
-    memcpy(result->palette, gif->palette, sizeof(gd_Palette));
-
-    // Allocate memory for canvas and frame
-    result->canvas = malloc(gif->width * gif->height);
-    if (result->canvas == NULL)
-    {
-        // Failed to allocate memory
-        free(result->palette);
-        free(result);
-        return NULL;
-    }
-    memcpy(result->canvas, gif->canvas, gif->width * gif->height);
-
-    result->frame = malloc(gif->width * gif->height);
-    if (result->frame == NULL)
-    {
-        // Failed to allocate memory
-        free(result->canvas);
-        free(result->palette);
-        free(result);
-        return NULL;
-    }
-    memcpy(result->frame, gif->frame, gif->width * gif->height);
-    */
+    GIF *result = (GIF *)PyObject_New(GIF, &PyGIF);
+    result->gd_GIF_ptr = gif;
+    // printf("in open fucntion the gd_GIF pointer is %d \n", result->gd_GIF_ptr);
     return (PyObject *)result;
 }
 
 static PyMethodDef methods[] = {
-    {"add", add, METH_VARARGS, "add functions"},
-    {"open_gif", open_gif, METH_VARARGS, "open_gif"},
+    {"open", open_gif, METH_VARARGS, "open a GIF object"},
     {NULL, NULL, 0, NULL}};
 
 static struct PyModuleDef pygifdec = {
