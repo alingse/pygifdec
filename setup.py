7a56e84df003e997f1a6e101137be4288e4557e5
alingse <alingse@foxmail.com>
2023-12-08 23:09:16 +0800

    add python setup.py config

--- setup.py
+++ setup.py
@@ -3,13 +3,19 @@ from distutils.core import setup, Extension
 pygifdec = Extension('pygifdec',
                      sources=['./src/pygifdec.c', './src/gifdec.c'],
                      include_dirs=['./src'],
-                     headers=["src/gifdec.h"],
-                     # extra_compile_args=['-g', '-shared', '-fPIC'],
                      )
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    readme = f.read()
 
 setup(name='pygifdec',
       version='0.0.2',
-      description='a pygifdec',
+      author='alingse',
+      author_email='alingse@foxmail.com',
+      description='a python gifdec wrapper',
+      long_description=readme,
+      long_description_content_type='text/markdown',
+      url='https://github.com/alingse/pygifdec',
       ext_modules=[pygifdec],
       packages=['src'],
       package_data={'src': ['gifdec.h']},
