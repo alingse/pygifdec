c1d661ab02808d65ccac8b812eea731741be7228
alingse <alingse@foxmail.com>
2023-12-09 01:00:09 +0800

    add more test

--- tests/test_gif.py
+++ tests/test_gif.py
@@ -3,4 +3,13 @@ import pygifdec
 
 def test_all():
     gif = pygifdec.open("cube.gif")
+    assert gif.width = 500
+    assert gif.height = 500
+    assert gif.size = 500 * 500 * 3
+
+    frame = bytearray(gif.size)
+    gif.render_frame(frame)
+    gif.get_frame()
+    gif.render_frame(frame)
+
     gif.close()
