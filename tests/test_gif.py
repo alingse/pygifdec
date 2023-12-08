1c642824d2ce81cfb06495c13ff35f033558184e
alingse <alingse@foxmail.com>
2023-12-09 01:02:22 +0800

    add timit

--- tests/test_gif.py
+++ tests/test_gif.py
@@ -3,9 +3,9 @@ import pygifdec
 
 def test_all():
     gif = pygifdec.open("cube.gif")
-    assert gif.width = 500
-    assert gif.height = 500
-    assert gif.size = 500 * 500 * 3
+    assert gif.width == 500
+    assert gif.height == 500
+    assert gif.size == 500 * 500 * 3
 
     frame = bytearray(gif.size)
     gif.render_frame(frame)
